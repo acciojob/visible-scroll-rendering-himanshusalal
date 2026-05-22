82112154168167b58c0f8033b4349645947fac4c
himanshusalal <himanshusalal517@gmail.com>
2026-05-22 12:24:41 +0530

    start

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,79 @@
+import React, { useRef, useState } from "react";
 
-import React from "react";
-import './../styles/App.css';
+function App() {
+  const totalItems = 1000;
+  const itemHeight = 50;
+  const containerHeight = 500;
 
-const App = () => {
+  const scrollRef = useRef(null);
+
+  const [scrollTop, setScrollTop] = useState(0);
+
+  // Total visible items
+  const visibleCount = Math.ceil(containerHeight / itemHeight);
+
+  // Start and End indexes
+  const startIndex = Math.floor(scrollTop / itemHeight);
+  const endIndex = startIndex + visibleCount + 2;
+
+  // Large list
+  const items = Array.from(
+    { length: totalItems },
+    (_, index) => `Item ${index + 1}`
+  );
+
+  // Scroll handler
+  const handleScroll = () => {
+    setScrollTop(scrollRef.current.scrollTop);
+  };
+
+  // Visible items only
+  const visibleItems = items.slice(startIndex, endIndex);
+
   return (
-    <div>
-        {/* Do not remove the main div */}
+    <div
+      ref={scrollRef}
+      onScroll={handleScroll}
+      style={{
+        height: "500px",
+        overflowY: "auto",
+        border: "2px solid black",
+        position: "relative",
+      }}
+    >
+      {/* Total height */}
+      <div
+        style={{
+          height: `${totalItems * itemHeight}px`,
+          position: "relative",
+        }}
+      >
+        {visibleItems.map((item, index) => {
+          const actualIndex = startIndex + index;
+
+          return (
+            <div
+              key={actualIndex}
+              style={{
+                height: `${itemHeight}px`,
+                position: "absolute",
+                top: `${actualIndex * itemHeight}px`,
+                left: 0,
+                right: 0,
+                borderBottom: "1px solid gray",
+                display: "flex",
+                alignItems: "center",
+                paddingLeft: "10px",
+                background: "white",
+              }}
+            >
+              {item}
+            </div>
+          );
+        })}
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
